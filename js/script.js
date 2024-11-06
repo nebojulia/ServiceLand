a0dfad2c5e6ff4b8ac493c7b5c19f71c0390e492
Julia Nebo <80979975+nebojulia@users.noreply.github.com>
2024-11-06 22:15:35 +0300

    feat: добавила js для блока menu

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   const burger = document.querySelector("#burger");
+  const menu = document.querySelector("#menu");
+  const close = document.querySelector("#close");
 
   if (burger) {
     burger.addEventListener("click", () => {
       document.body.classList.toggle("open-nav");
+      menu.classList.toggle("menu-open");
+    });
+  }
+
+  if (close) {
+    close.addEventListener("click", () => {
+      document.body.classList.toggle("open-nav");
+      menu.classList.toggle("menu-open");
     });
   }
 });
