b18d59ab6e3943f5cf68cb8494bcb310822d1fff
Julia Nebo <80979975+nebojulia@users.noreply.github.com>
2024-11-16 12:16:35 +0300

    feat: правки блока menu

--- js/script.js
+++ js/script.js
@@ -2,22 +2,17 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   const burger = document.querySelector("#burger");
-  const menu = document.querySelector("#menu");
   const close = document.querySelector("#close");
 
   if (burger) {
     burger.addEventListener("click", () => {
       document.body.classList.toggle("open-nav");
-      menu.classList.toggle("menu-open");
-      document.querySelector(".page").classList.toggle("menu__blur");
     });
   }
 
   if (close) {
     close.addEventListener("click", () => {
       document.body.classList.toggle("open-nav");
-      menu.classList.toggle("menu-open");
-      document.querySelector(".page").classList.toggle("menu__blur");
     });
   }
 });
