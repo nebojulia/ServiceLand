f66d1ab3884aba8e25b48485aff8949ffa86edb1
Julia Nebo <80979975+nebojulia@users.noreply.github.com>
2024-12-02 19:24:43 +0300

    feat: добавила stylelint

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const burger = document.querySelector("#burger");
   const close = document.querySelector("#close");
+  const menu = document.querySelector("#menu");
 
   if (burger) {
     burger.addEventListener("click", () => {
@@ -14,5 +15,19 @@ document.addEventListener("DOMContentLoaded", function () {
     close.addEventListener("click", () => {
       document.body.classList.toggle("open-nav");
     });
+  }
+
+  if (document.body.classList === "open-nav") {
+    document.onclick = function (event) {
+      let target = event.target;
+
+      if (event.target.classList !== "page__menu") {
+        closeMenu();
+      }
+
+      function closeMenu() {
+        document.body.classList.toggle("open-nav");
+      }
+    };
   }
 });
